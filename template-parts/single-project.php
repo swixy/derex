26c5f35c172bcbfb47697c807e02a4f8fb28b114
vunterberg <vunterberg@azds.com>
2024-03-28 10:56:12 +0300

    start single page

---
+++ template-parts/single-project.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Single project template
+ * php version 8.3.3
+ * 
+ * @category Template
+ * @package  Wordpress
+ * @author   vunterberg <vladunter404@gmail.com>
+ * @license  GNU Public License
+ * @link     https://developer.wordpress.org/themes/template-files-section/page-template-files/
+ */
+?>
+
+<section class="project">
+    <div class="container">
+        <h1 class="project__title h1">
+            Project name
+        </h1>
+        <p class="text--default">
+        Lorem ipsum dolor sit amet consectetur. Nulla lorem pharetra enim nec. Diam dignissim quam vulputate sed enim. Nulla lorem pharetra enim nec.
+        </p>
+        <img class="project__poster" src="<?php echo get_template_directory_uri();?>/src/image/single-project-holder.jpg" alt="">
+    </div>
+</section>
